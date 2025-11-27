d8b9dee57151b7c18d26e1d1c0aed797341cb441
root <engelerclaudio9@gmail.com>
2025-11-27 16:58:47 +0000

    Added subslot randomizer

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,22 +1,34 @@
 $("#randomizeButton").click(function(){
-    randomProt = protectors[Math.floor(Math.random() * protectors.length)];
 
+    randomProt = "";
+    randomProtSub = "";
+
+    while (randomProt == randomProtSub) {
+        randomProt = protectors[Math.floor(Math.random() * protectors.length)];
+        randomProtSub = protectors[Math.floor(Math.random() * protectors.length)];
+    }
 
     imgPath = `/img/Characters/Protectors/${randomProt}.png`;
+    imgPathSubslot = `/img/Characters/ProtectorsSub/${randomProtSub}.png`;
 
-    $('#protectorSlot').html(`<img src="${imgPath}" width=100%>`);
+    $('#protectorSlot').html(`<img src="${imgPath}" class="image-a"><img src="${imgPathSubslot}" class="image-b">`);
 
     usedBU = [];
     index = 1;
 
     while(index < 6) {
         randomBU = battleUnits[Math.floor(Math.random() * battleUnits.length)];
+        randomBUSubslot = battleUnits[Math.floor(Math.random() * battleUnits.length)];
 
-        if(!usedBU.includes(randomBU)) {
-            usedBU.push(randomBU);
-            imgPath = `/img/Characters/PC/${randomBU}.png`;
-            $('#battleUnit' + index).html(`<img src="${imgPath}" width=100%>`);
-            index++;
+        if(randomBU != randomBUSubslot) {
+            if(!usedBU.includes(randomBU) && !usedBU.includes(randomBUSubslot)) {
+                usedBU.push(randomBU);
+                usedBU.push(randomBUSubslot);
+                imgPath = `/img/Characters/BattleUnits/${randomBU}.png`;
+                imgPathSubslot = `/img/Characters/BattleUnitsSub/${randomBUSubslot}.png`;
+                $('#battleUnit' + index).html(`<img src="${imgPath}" class="image-a"><img src="${imgPathSubslot}" class="image-b">`);
+                index++;
+            }
         }
     }
 });

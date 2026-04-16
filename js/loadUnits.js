fcc622555aa657b1d032730db96f716b0ad64b86
iSim7 <engelerclaudio9@gmail.com>
2026-04-16 22:27:01 +0200

    Implemented new randomizer algo

--- js/loadUnits.js
+++ js/loadUnits.js
@@ -1,10 +1,25 @@
 var protectors = [];
 var battleUnits = [];
 
+function shuffle(array) {
+  // We loop backwards from the last element to the first
+  for (let i = array.length - 1; i > 0; i--) {
+    // Pick a random index from 0 to i
+    const j = Math.floor(Math.random() * (i + 1));
+    
+    // Swap elements array[i] and array[j]
+    // We use "destructuring assignment" for a clean swap
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 $.get('../other/Protectors.txt', function(data) {
     protectors = data.split("\n");
+    protectors = shuffle(protectors);
 }, 'text');
 
 $.get('../other/BattleUnits.txt', function(data) {
     battleUnits = data.split("\n");
+    battleUnits = shuffle(battleUnits);
 }, 'text');
